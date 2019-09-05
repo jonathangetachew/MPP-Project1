19ec73a150d20d371246630be233ef4020ce2039
Jonathan Getachew <jonathangetachew@gmail.com>
2019-09-04 20:56:13 -0500

    [CA-2] Generated a Spring Boot 2 project.

---
+++ twitter-clone-server/src/test/java/com/mpp/twittercloneserver/TwitterCloneServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mpp.twittercloneserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TwitterCloneServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
