63cf14942323329868927e0eafa5450feee77145
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-09-10 16:45:52 -0500

    Added CommonNavigator

--- twitter-clone-client/navigation/AppNavigator.js
+++ twitter-clone-client/navigation/AppNavigator.js
@@ -1,6 +1,4 @@
-import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import CommonNavigator from './CommonNavigator';
-
 
 export default createAppContainer(CommonNavigator);
