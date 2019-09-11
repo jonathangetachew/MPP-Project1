842ec3249bf5346babe6d9c192e514be447d13c2
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-09-10 21:19:33 -0500

    [CA-21] Added Redux support to the application.

---
+++ twitter-clone-client/src/redux/sagas/tweets.js
@@ -0,0 +1,43 @@
+import { call, put, takeLatest } from 'redux-saga/effects';
+import constants from '../actions/tweets';
+import { store } from '../store';
+import tweetServiceMock from '../services/tweet.service.mock';
+
+function* items(action) {
+    yield put({ type: constants.GET_TWEETS_REQUEST });
+    try {
+        const data = yield call(tweetServiceMock.getTweets);
+        yield put({ type: constants.GET_TWEETS_SUCCESS, data: data.map((item, key) => !item.id ? { ...item, id: key } : item) });
+    } catch (error) {
+        let errorMessage = 'Error when retrieving items';
+        if (error && error.message) {
+            errorMessage = error.message;
+        }
+
+        yield put({ type: constants.GET_TWEETS_FAIL, error: errorMessage });
+    }
+}
+
+function* searchItems(action) {
+    yield put({ type: constants.SEARCH_TWEETS_REQUEST });
+
+    try {
+        const data = yield call(tweetServiceMock.search, action.criteria);        
+        yield put({ type: constants.SEARCH_TWEETS_SUCCESS, data: data.map((item, key) => !item.id ? { ...item, id: key } : item) });
+    } catch (error) {
+
+        let errorMessage = 'Error when retrieving items';
+        if (error.message.includes("504")) {
+            errorMessage = "Error with the criteria. Must be alphanum."
+        } else if (error && error.message) {
+            errorMessage = error.message;
+        }
+
+        yield put({ type: constants.SEARCH_TWEETS_FAIL, error: errorMessage });
+    }
+}
+
+export default [
+    takeLatest(constants.GET_TWEETS, items),
+    takeLatest(constants.SEARCH_TWEETS, searchItems),
+];
