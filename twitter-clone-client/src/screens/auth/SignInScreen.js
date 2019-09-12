431b7ba284af65029ee637ab8adc933912e925ac
unknown <610580@CS.MUM.EDU>
2019-09-12 11:18:32 -0500

    Some changes to SignInScreen

--- twitter-clone-client/src/screens/auth/SignInScreen.js
+++ twitter-clone-client/src/screens/auth/SignInScreen.js
@@ -1,15 +1,20 @@
 import React from "react";
 import {
   AsyncStorage,
-  Button,
-  StyleSheet,
   View,
+  ScrollView,
+  KeyboardAvoidingView,
   Text,
-  TextInput
 } from "react-native";
-import { Input, ThemeProvider, CheckBox, Image } from "react-native-elements";
+import {
+  Input,
+  CheckBox,
+  Image
+} from "react-native-elements";
+import RoundButton from "../../components/RoundButton";
+import Colors from "../../constants/Colors";
 import Icon from "react-native-vector-icons/FontAwesome";
-import LogoTitle from "../../components/LogoTitle"
+import imageLogo from "../../../assets/images/logo.png";
 
 export default class SignInScreen extends React.Component {
   state = {
@@ -20,101 +25,97 @@ export default class SignInScreen extends React.Component {
     rememberMe: false
   };
   static navigationOptions = {
-    headerTitle: <LogoTitle title="Please sign in"/>
+    header: null
   };
 
   render() {
     return (
-      <ThemeProvider>
-        <View style={{ flex: 1, padding: 100 }}>
-          <View
-            style={{ flex: 1, flexDirection: "column", alignItems: "center" }}
-          >
-            <Image
-              source={
-                require('../../../assets/images/nyan1.svg')
-              }
-              style={this.styles.welcomeImage}
-            />
-            <Text style={{ fontSize: 27 }}>Log in to Twitter Clone</Text>
-            <Input
-              label="Username"
-              autoFocus={true}
-              errorMessage={this.state.usernameError}
-              leftIcon={
-                <Icon
-                  name="user"
-                  size={24}
-                  color="black"
-                  style={{ paddingRight: 2 }}
-                />
-              }
-              textContentType="username"
-              onChangeText={value => this.setState({ username: value })}
-              value={this.state.value}
-            />
-            <Input
-              label="Password"
-              errorMessage={this.state.passwordError}
-              textContentType="password"
-              autoCompleteType="password"
-              secureTextEntry={true}
-              onChangeText={value => this.setState({ password: value })}
-            />
-            <View style={{ margin: 7 }} />
+      <ScrollView contentContainerStyle={this.styles.container}>
+        <View style={this.styles.container}>
+        <Image source={imageLogo} style={this.styles.logo} />
+        <KeyboardAvoidingView style={this.styles.form}>
+          <Text style={{ fontSize: 27, marginBottom: 20 }}>Log in to TwitterClone</Text>
+          <Input
+            label="Username"
+            autoFocus={true}
+            errorMessage={this.state.usernameError}
+            leftIcon={
+              <Icon
+                name="user"
+                size={24}
+                color={Colors.primaryColor}
+                style={{ marginRight: 8 }}
+              />
+            }
+            maxLength={20}
+            textContentType="username"
+            onChangeText={value => this.setState({ username: value })}
+            value={this.state.value}
+          />
+          <Input
+            label="Password"
+            errorMessage={this.state.passwordError}
+            leftIcon={
+              <Icon
+                name="lock"
+                size={24}
+                color={Colors.primaryColor}
+                style={{ marginRight: 8 }}
+              />
+            }
+            textContentType="password"
+            autoCompleteType="password"
+            secureTextEntry={true}
+            onChangeText={value => this.setState({ password: value })}
+          />
+          <View style={{ margin: 7 }} />
 
-            <View style={{flexDirection: "column"}}>
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "baseline"
-                }}
-              >
-                <Button
-                  title="Log in!"
-                  style={{ flex: 2 }}
-                  onPress={this._signInAsync}
-                />
-                <CheckBox
-                  style={{ flex: 1 }}
-                  checked={this.state.rememberMe}
-                  onPress={() =>
-                    this.setState({ rememberMe: !this.state.rememberMe })
-                  }
-                  title="Remember me"
-                ></CheckBox>
-                <Text
-                  style={{ color: "blue", flex: 1 }}
-                  onPress={() =>
-                    this.props.navigation.navigate("ForgotPassword")
-                  }
-                >
-                  Forgot Password
-                </Text>
-              </View>
+          <View style={{ flexDirection: "column" }}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "baseline"
+              }}
+            >
+              <RoundButton
+                title="Log in!"
+                style={{ flex: 2 }}
+                onPress={this._signInAsync}
+              />
+              <CheckBox
+                style={{ flex: 1 }}
+                checked={this.state.rememberMe}
+                onPress={() =>
+                  this.setState({
+                    rememberMe: !this.state.rememberMe
+                  })
+                }
+                title="Remember me"
+              ></CheckBox>
+            </View>
 
-              <View
-                style={{
-                  flex: 1,
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  alignItems: "center"
-                }}
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center"
+              }}
+            >
+              <Text style={this.styles.spacedThingyForJonathansEyes}>New to TwitterClone? </Text>
+              <Text
+                style={{ color: "blue" }}
+                onPress={() => this.props.navigation.navigate("Signup")}
               >
-                <Text style={{ flex: 1 }}>New to Twitter Clone? </Text>
-                <Text
-                  style={{ color: "blue" }}
-                  onPress={() => this.props.navigation.navigate("Signup")}
-                >
-                  Sign up now!
-                </Text>
-              </View>
+                Sign up
+              </Text>
             </View>
           </View>
+        </KeyboardAvoidingView>
         </View>
-      </ThemeProvider>
+      </ScrollView>
     );
   }
 
@@ -129,19 +130,42 @@ export default class SignInScreen extends React.Component {
     if (errorObject.usernameError != null || errorObject.passwordError != null)
       this.setState(errorObject);
     else {
-      this.setState({ usernameError: null, passwordError: null });
+      this.setState({
+        usernameError: null,
+        passwordError: null
+      });
       await AsyncStorage.setItem("userToken", "abc");
       this.props.navigation.navigate("App");
     }
   };
 
   styles = {
-  welcomeImage: {
-    width: 120,
-    height: 80,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  }
-  }
+    moreSpacedThingyForJonathansEyes: {
+      marginTop: 15,
+      marginBottom: 15
+    },
+    spacedThingyForJonathansEyes: {
+      marginTop: 15,
+      marginBottom: 15
+    },
+    container: {
+      flex: 1,
+      backgroundColor: "white",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginTop: 30 //"3rem"
+    },
+    form: {
+      flex: 1,
+      flexDirection: "column",
+      alignItems: "center"
+    },
+    logo: {
+      width: 95,
+      height: 80,
+      resizeMode: "contain",
+      marginTop: 30
+      //marginLeft: -10,
+    }
+  };
 }
